ff12057778c51e4f3cbd8470af5e27a2939e8dd9
kiddomute <hellotalkwp@gmail.com>
2026-02-02 07:37:54 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,19 +3,10 @@ from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filte
 
 TOKEN = "8140457471:AAEUYRqq2J6ZiX71BqYmqXUGeQYp-6ZVwJM"
 
-REPLY_TEXT = "ဖာသည်မနှင်းဦးဝေ"
-
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # delete user's message
-    try:
-        await update.message.delete()
-    except:
-        pass
-
-    # send fixed reply
-    await update.message.chat.send_message(REPLY_TEXT)
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ဖာသည်မနှင်းဦးဝေ")
+    await update.message.delete()
 
 app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
+app.add_handler(MessageHandler(filters.ALL, reply))
 app.run_polling()
